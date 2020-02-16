e55a7f44bee5a51edb8c100b17b1a2674e65cd68
hd2eng <Administrator@YUNAW-20140805Y>
2020-02-16 10:36:04 +0800

    Signed-off-by: hd2eng <Administrator@YUNAW-20140805Y>

---
+++ src/dd2/HelloJava.java
@@ -0,0 +1,14 @@
+package dd2;
+
+public class HelloJava {
+
+	public static void main(String[] args) {
+		//实现打印输出
+		System.out.println("你好！");
+		System.out.println("今天您吃了吗");
+		System.out.println("吃了，您呢？");
+		System.out.println("还好！！！");	
+		System.out.println("今天心情还不错！！！");	
+	}
+
+}
